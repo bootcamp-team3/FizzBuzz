8bb45313edfa47b15fcbd995a683815f20bd3236
Dongwook_Min <48678252+mindw96@users.noreply.github.com>
2024-01-15 18:09:54 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 def print_fizz(num):
-		if i % 3 == 0:
-		    print('fizz')
+    if i % 3 == 0:
+        print('fizz')
 
 def print_buzz(num):
     if num % 5 == 0:
@@ -14,8 +14,8 @@ def print_fizzbuzz(num):
 if __name__ == '__main__':
     for i in range(1,21):
         if i % 15 == 0:
-            print(print_fizzbuzz(i))
+            print_fizzbuzz(i)
         elif i % 3 == 0:
-            print(print_fizz(i))
+            print_fizz(i)
         elif i % 5 == 0:
-            print(print_buzz(i))
+            print_buzz(i)
